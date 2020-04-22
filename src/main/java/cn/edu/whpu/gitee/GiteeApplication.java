1fa667341caae76fc5b9ae2690129708ec772990
yangli <2375331053@qq.com>
2020-04-22 10:30:23 +0800

    第一次提交

---
+++ src/main/java/cn/edu/whpu/gitee/GiteeApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.whpu.gitee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiteeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/whpu/gitee/GiteeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.whpu.gitee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
